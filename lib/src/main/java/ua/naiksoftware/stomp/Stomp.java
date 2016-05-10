2b0175f3918dbbd25a9a45cc5ef859c6eb4922d4
naik <naiksav4enko@gmail.com>
2016-05-10 10:15:06 +0300

    minSdk 16, small refactoring

--- lib/src/main/java/ua/naiksoftware/stomp/Stomp.java
+++ lib/src/main/java/ua/naiksoftware/stomp/Stomp.java
@@ -1,12 +1,12 @@
 package ua.naiksoftware.stomp;
 
-import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.WebSocket;
 
 import ua.naiksoftware.stomp.client.StompClient;
 
 /**
  * Supported overlays:
- *  - org.java_websocket.client.WebSocketClient ('org.java-websocket:Java-WebSocket:1.3.0')
+ *  - org.java_websocket.WebSocket ('org.java-websocket:Java-WebSocket:1.3.0')
  *
  *  You can add own relay, just implement ConnectionProvider for you stomp transport,
  *  such as web socket.
@@ -16,7 +16,7 @@ import ua.naiksoftware.stomp.client.StompClient;
 public class Stomp {
 
     public static StompClient over(Class claszz, String uri) {
-        if (claszz == WebSocketClient.class) {
+        if (claszz == WebSocket.class) {
             return createStompClient(new WebSocketsConnectionProvider(uri));
         }
 
