c565b247906045934d9712f4e0cf5a9f90cfc234
naik <naiksav4enko@gmail.com>
2016-09-20 16:47:30 +0300

    Add client module

---
+++ example-client/src/main/java/ua/naiksoftware/exampleclient/MainActivity.java
@@ -0,0 +1,14 @@
+package ua.naiksoftware.exampleclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by naik on 20.09.16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
