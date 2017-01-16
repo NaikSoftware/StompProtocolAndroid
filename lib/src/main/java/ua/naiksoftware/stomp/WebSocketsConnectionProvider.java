1f12ed2732331785b4250603a1e32e1925b89aee
Khoa Tran <khoa.tran@castar.com>
2017-01-16 14:49:10 -0800

    Correctly initialize SSLContext with TrustManager

--- lib/src/main/java/ua/naiksoftware/stomp/WebSocketsConnectionProvider.java
+++ lib/src/main/java/ua/naiksoftware/stomp/WebSocketsConnectionProvider.java
@@ -114,9 +114,7 @@ public class WebSocketsConnectionProvider implements ConnectionProvider {
 
         if(mUri.startsWith("wss")) {
             try {
-                SSLContext sc = SSLContext.getInstance("TLS");
-                sc.init(null, null, null);
-                mWebSocketClient.setWebSocketFactory(new DefaultSSLWebSocketClientFactory(sc));
+                mWebSocketClient.setWebSocketFactory(new DefaultSSLWebSocketClientFactory(getSslContext()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -163,5 +161,34 @@ public class WebSocketsConnectionProvider implements ConnectionProvider {
                 if (iterator.next().isUnsubscribed()) iterator.remove();
             }
         });
+    }
+
+    private SSLContext getSslContext() {
+        TrustManager[] byPassTrustManagers = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+            }
+        } };
+
+        SSLContext sslContext=null;
+
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            sslContext.init(null, byPassTrustManagers, new SecureRandom());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        return sslContext;
     }
 }
