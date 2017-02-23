6bed15604b051bf0c8a8aecfde2d5c034f4c8299
NaikSoftware <naiksav4enko@gmail.com>
2017-02-23 18:07:55 +0200

    Fix build example

--- example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
+++ example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
@@ -3,8 +3,6 @@ package ua.naiksoftware.stompclientexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import ua.naiksoftware.stomp.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
