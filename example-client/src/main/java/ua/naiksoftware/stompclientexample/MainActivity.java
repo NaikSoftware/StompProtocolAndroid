7ac5e2eef0d512395a4d94c04584d91c0b4b5e39
NaikSoftware <naiksav4enko@gmail.com>
2017-02-24 11:54:14 +0200

    Connect to STOMP working

--- example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
+++ example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
@@ -2,12 +2,86 @@ package ua.naiksoftware.stompclientexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.java_websocket.WebSocket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.client.StompClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    public static final String ANDROID_EMULATOR_LOCALHOST = "10.0.2.2";
+
+    private SimpleAdapter mAdapter;
+    private List<String> mDataSet = new ArrayList<>();
+    private StompClient mStompClient;
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mAdapter = new SimpleAdapter(mDataSet);
+        mAdapter.setHasStableIds(true);
+        recyclerView.setAdapter(mAdapter);
+
+        connectStomp();
+    }
+
+    private void connectStomp() {
+        mStompClient = Stomp.over(WebSocket.class, "ws://" + ANDROID_EMULATOR_LOCALHOST + ":8080/example-endpoint/websocket");
+        mStompClient.lifecycle()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            toast("Stomp connection opened");
+                            break;
+                        case ERROR:
+                            Log.e(TAG, "Stomp connection error", lifecycleEvent.getException());
+                            toast("Stomp connection error");
+                            break;
+                        case CLOSED:
+                            toast("Stomp connection closed");
+                    }
+                });
+
+        // Receive greetings
+        mStompClient.topic("/topic/greetings")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(topicMessage -> {
+                    Log.d(TAG, "Received " + topicMessage);
+                    addItem(topicMessage.getPayload());
+                });
+
+        mStompClient.connect();
+    }
+
+    private void addItem(String text) {
+        mDataSet.add(++counter + text);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void toast(String text) {
+        Log.i(TAG, text);
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mStompClient.disconnect();
+        super.onDestroy();
     }
 }
