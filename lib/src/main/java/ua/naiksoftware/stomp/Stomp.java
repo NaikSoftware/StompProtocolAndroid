46170cad28e02c0f02a3115c915ba481b023bc25
Francois Lolom <francois.lolom@gmail.com>
2017-05-30 11:14:08 +0200

    Add a new connection provider: OkHttp

--- lib/src/main/java/ua/naiksoftware/stomp/Stomp.java
+++ lib/src/main/java/ua/naiksoftware/stomp/Stomp.java
@@ -9,6 +9,7 @@ import ua.naiksoftware.stomp.client.StompClient;
 /**
  * Supported overlays:
  *  - org.java_websocket.WebSocket ('org.java-websocket:Java-WebSocket:1.3.0')
+ *  - okhttp3.WebSocket ('com.squareup.okhttp3:okhttp:3.8.0')
  *
  *  You can add own relay, just implement ConnectionProvider for you stomp transport,
  *  such as web socket.
@@ -31,6 +32,8 @@ public class Stomp {
     public static StompClient over(Class clazz, String uri, Map<String, String> connectHttpHeaders) {
         if (clazz == WebSocket.class) {
             return createStompClient(new WebSocketsConnectionProvider(uri, connectHttpHeaders));
+        } else if (clazz == okhttp3.WebSocket.class) {
+            return createStompClient(new OkHttpConnectionProvider(uri, connectHttpHeaders));
         }
 
         throw new RuntimeException("Not supported overlay transport: " + clazz.getName());
